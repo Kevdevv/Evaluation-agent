50f9cc1eb1cf861e9bde5ff6d31b9486297a67e1
Kevin Rama <ramanantsoa.vlm@gmail.com>
2022-07-19 12:52:54 +0200

    ajout panneau administration

--- src/Controller/AdminMissionsController.php
+++ src/Controller/AdminMissionsController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use App\Entity\Missions;
 use App\Form\MissionsType;
 use App\Repository\MissionsRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/missions')]
 class AdminMissionsController extends AbstractController
